1d8064346c121b81e16903a5a137670c201294b4
zouwei <huzougege@gmail.com>
2026-03-02 14:35:05 +0800

    fix:update path

--- Casks/moraya.rb
+++ Casks/moraya.rb
@@ -2,10 +2,10 @@ cask "moraya" do
   arch arm: "aarch64", intel: "x64"
 
   version "0.16.1"
-  sha256 arm:   "REPLACE_WITH_AARCH64_SHA256",
-         intel: "REPLACE_WITH_X64_SHA256"
+  sha256 arm:   "77dcab6d2e51d63bf5319fee6a88dae1386d9642c12a44e61d63c0931eb67e4d",
+         intel: "1103fcd2ef93ce5036ba43e968d10fb6f5bd7167b94c559241a9a4c4e83671c6"
 
-  url "https://github.com/zouwei/moraya/releases/download/v#{version}/Moraya_#{version}_#{arch}.dmg",
+  url "https://github.com/zouwei/moraya/releases/download/v#{version}/Moraya_#{version}_mac_#{arch}.dmg",
       verified: "github.com/zouwei/moraya/"
 
   name "Moraya"
